3a8c91e845ebb2b424ac2cc2c2167fcd2bbca60c
Ayomide6441 <onifadeayomide328@gmail.com>
2024-06-06 23:19:34 +0100

    fix: correct tab styling

--- src/pages/Home/ProductList.tsx
+++ src/pages/Home/ProductList.tsx
@@ -5,9 +5,19 @@ import LatestList from "./LatestList";
 function ProductList() {
   return (
     <Tabs defaultValue="featured" className="container mb-24">
-      <TabsList className="mb-12 flex justify-center">
-        <TabsTrigger value="featured">Featured</TabsTrigger>
-        <TabsTrigger value="latest">Latest</TabsTrigger>
+      <TabsList className="mb-12 flex justify-center bg-transparent gap-3">
+        <TabsTrigger
+          className="focus:border focus:px-6 focus:rounded-full shadow-none"
+          value="featured"
+        >
+          Featured
+        </TabsTrigger>
+        <TabsTrigger
+          className="focus:border focus:px-6 focus:rounded-full shadow-none"
+          value="latest"
+        >
+          Latest
+        </TabsTrigger>
       </TabsList>
       <TabsContent value="featured">
         <FeaturedList />
