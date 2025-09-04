b77ff0f918ef805b1e3075ad40117557f9bbfc27
Ayomide <onifadeayomide328@gmail.com>
2025-09-04 10:28:57 +0100

    feat: add hover feature to the ProductItem.tsx

--- src/components/ui/ProductItem.tsx
+++ src/components/ui/ProductItem.tsx
@@ -1,4 +1,6 @@
+import { Button } from "./button";
 import { Text } from "./text";
+import { Heart, ShoppingCart } from "lucide-react";
 
 type ProductItemProps = {
   image: string;
@@ -9,17 +11,48 @@ type ProductItemProps = {
 
 function ProductItem({ image, productName, inStock, price }: ProductItemProps) {
   return (
-    <div className="flex flex-col gap-3 w-full max-w-[17rem] mx-auto">
-      <div className="bg-[#F5F5F5] flex justify-center items-center rounded">
+    <div className="flex flex-col gap-3 w-full max-w-[17rem] mx-auto relative group">
+      {/* Heart (Like button) */}
+      <Button
+        variant="ghost"
+        className="absolute top-4 right-4 z-20 transform scale-90 opacity-0 
+        group-hover:scale-100 group-hover:opacity-100 transition-all duration-300 ease-out"
+      >
+        <Heart className="cursor-pointer" />
+      </Button>
+
+      {/* Product Image + Overlay + Add to cart */}
+      <div className="bg-[#F5F5F5] flex justify-center items-center rounded relative overflow-hidden">
+        {/* Product Image */}
         <img
           src={image}
           alt={productName}
           width={234}
           height={312}
-          className="object-contain p-4"
+          className="object-contain p-4 relative z-10"
         />
+
+        {/* Overlay */}
+        <div
+          className="absolute inset-0 bg-[#F5F5F5]/60 opacity-0 group-hover:opacity-100 
+          transition-opacity duration-500 ease-out z-10"
+        ></div>
+
+        {/* Add to Cart button */}
+        <span
+          className="absolute bottom-[-100%] w-full z-20 
+          group-hover:bottom-0 transition-all duration-300 ease-out"
+        >
+          <Button className="w-full gap-2 rounded-t-none">
+            Add to cart
+            <span>
+              <ShoppingCart className="w-4 h-4" />
+            </span>
+          </Button>
+        </span>
       </div>
 
+      {/* Product Info */}
       <Text variant="body-2" className="truncate">
         {productName}
       </Text>
