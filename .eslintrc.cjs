e0116b94f9751f083c0ab89a7afa002969ceccc0
Ayomide <onifadeayomide328@gmail.com>
2025-10-03 09:57:29 +0100

    chore: update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,53 @@
+// module.exports = {
+//   root: true,
+//   env: { browser: true, es2020: true },
+//   extends: [
+//     'eslint:recommended',
+//     'plugin:@typescript-eslint/recommended',
+//     'plugin:react-hooks/recommended',
+//   ],
+//   ignorePatterns: ['dist', '.eslintrc.cjs'],
+//   parser: '@typescript-eslint/parser',
+//   plugins: ['react-refresh'],
+//   rules: {
+//     'react-refresh/only-export-components': [
+//       'warn',
+//       { allowConstantExport: true },
+//     ],
+//   },
+// }
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react", "react-hooks", "react-refresh", "@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    // optional: your own overrides
   },
-}
+  settings: {
+    react: {
+      version: "detect", // automatically detects React version
+    },
+  },
+};
