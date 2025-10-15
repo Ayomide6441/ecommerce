503af4aa79a7e63d7a95904790d7c68e4efe0c82
Ayomide <onifadeayomide328@gmail.com>
2025-10-15 15:23:06 +0100

    refactor(layout): enhance AppLayout structure for improved styling

--- apps/admin/src/components/layout/app-layout.tsx
+++ apps/admin/src/components/layout/app-layout.tsx
@@ -4,16 +4,14 @@ import Sidebar from "./sidebar";
 
 function AppLayout() {
   return (
-    <div className="flex gap-0 w-full h-screen">
-      <aside className="w-64 bg-W900 ">
+    <div className="flex gap-0 w-full min-h-screen">
+      <aside className="w-64 bg-W900 h-screen sticky top-0">
         <Sidebar />
       </aside>
       <div className="flex-1 bg-[#F5F5F5]">
         <Nav />
-        <main className="mt-10 px-32">
-          <div className="container bg-white rounded-md">
-            <Outlet />
-          </div>
+        <main className="mt-10 px-32 rounded-md">
+          <Outlet />
         </main>
       </div>
     </div>
