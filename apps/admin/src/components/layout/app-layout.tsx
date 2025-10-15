73052e2951fda8d6c6b9f76b84ba9375f00a0bc6
Ayomide <onifadeayomide328@gmail.com>
2025-10-15 10:54:03 +0100

    style(layout): improve admin app layout structure

--- apps/admin/src/components/layout/app-layout.tsx
+++ apps/admin/src/components/layout/app-layout.tsx
@@ -5,16 +5,17 @@ import Sidebar from "./sidebar";
 function AppLayout() {
   return (
     <div className="flex gap-0 w-full h-screen">
-      <section className="w-64 bg-W900 ">
+      <aside className="w-64 bg-W900 ">
         <Sidebar />
-      </section>
-      <main className="flex-1 bg-[#F5F5F5]">
+      </aside>
+      <div className="flex-1 bg-[#F5F5F5]">
         <Nav />
-
-        <div className="container mt-10 bg-white rounded-md">
-          <Outlet />
-        </div>
-      </main>
+        <main className="mt-10 px-32">
+          <div className="container bg-white rounded-md">
+            <Outlet />
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
